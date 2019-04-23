703a06ace0fa9930193aa900d708205ea059cac2
Andriy Gnennyy <andriy.gnennyy@globallogic.com>
2019-04-23 14:44:07 +0300

    Firecracker download and setup

--- src/scripts/fc-setup.sh
+++ src/scripts/fc-setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # get & setup binary
-curl -LOJ https://github.com/firecracker-microvm/firecracker/releases/download/v0.12.0/firecracker-v0.12.0
-mv firecracker-v0.12.0 firecracker
+curl -LOJ https://github.com/firecracker-microvm/firecracker/releases/download/v0.15.0/firecracker-v0.15.0
+mv firecracker-v0.15.0 firecracker
 chmod +x ./firecracker
 sudo mv ./firecracker /usr/local/bin/
 firecracker --version
@@ -11,5 +11,6 @@ firecracker --version
 sudo setfacl -m u:${USER}:rw /dev/kvm
 
 # get sample rootfs and kernel
-curl -fsSL -o simple-rootfs.ext4 https://s3.amazonaws.com/spec.ccfc.min/img/hello/fsfiles/hello-rootfs.ext4
-curl -fsSL -o vmlinux.bin https://s3.amazonaws.com/spec.ccfc.min/img/hello/kernel/hello-vmlinux.bin
+mkdir images
+curl -fsSL -o ./images/simple-rootfs.ext4 https://s3.amazonaws.com/spec.ccfc.min/img/hello/fsfiles/hello-rootfs.ext4
+curl -fsSL -o ./images/vmlinux.bin https://s3.amazonaws.com/spec.ccfc.min/img/hello/kernel/hello-vmlinux.bin
